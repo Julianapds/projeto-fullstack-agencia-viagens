ea8f4233f990c07f8dd0831f270fae5ad7b27487
Juliana Pires <=>
2023-11-02 22:44:40 -0300

    configuracao inicial do projeto

---
+++ src/main/java/com/rotasdosol/controller/MeuServlet.java
@@ -0,0 +1,15 @@
+package com.rotasdosol.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MeuServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/views/minhaView.jsp").forward(request, response);
+    }
+
+}
